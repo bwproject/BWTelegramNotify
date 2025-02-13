2004d9016710047135c2bf70721717b19276008f
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-13 23:21:26 +0500

    Update BWTelegramNotifyPaper.java

--- src/main/java/me/projectbw/BWTelegramNotifyPaper.java
+++ src/main/java/me/projectbw/BWTelegramNotifyPaper.java
@@ -1,30 +1,37 @@
 package me.projectbw;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BWTelegramNotifyPaper extends JavaPlugin {
+public class BWTelegramNotifyPaper extends JavaPlugin implements Listener {
     private TelegramSender telegramSender;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        FileConfiguration config = getConfig();
-        telegramSender = new TelegramSender(config);
-
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[PROJECTBW.RU] " + ChatColor.YELLOW + "BWTelegramNotify " + ChatColor.DARK_GREEN + "Активен");
-
-        telegramSender.sendMessage("⚡ Paper сервер запущен!");
-
-        getCommand("status").setExecutor(new StatusCommand(telegramSender));
-
-        Bukkit.getPluginManager().registerEvents(new PlayerListener(telegramSender), this);
+        this.telegramSender = new TelegramSender(
+            getConfig().getString("telegram.bot_token"), 
+            getConfig().getStringList("telegram.chat_ids")
+        );
+        
+        getServer().getPluginManager().registerEvents(this, this);
+        telegramSender.sendMessage(getConfig().getString("server.startup_message"));
     }
 
-    @Override
-    public void onDisable() {
-        telegramSender.sendMessage("⛔ Paper сервер выключен!");
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        telegramSender.sendMessage(getConfig().getString("server.player_join_message").replace("{player}", event.getPlayer().getName()));
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        telegramSender.sendMessage(getConfig().getString("server.player_quit_message").replace("{player}", event.getPlayer().getName()));
+    }
+
+    public void onServerShutdown() {
+        telegramSender.sendMessage(getConfig().getString("server.server_status_message.off"));
     }
 }
