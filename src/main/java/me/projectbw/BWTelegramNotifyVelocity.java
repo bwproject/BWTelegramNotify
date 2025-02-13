4d229003153aed49c1be8c0d9c0ded16c5e87076
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-13 23:21:09 +0500

    Update BWTelegramNotifyVelocity.java

--- src/main/java/me/projectbw/BWTelegramNotifyVelocity.java
+++ src/main/java/me/projectbw/BWTelegramNotifyVelocity.java
@@ -1,52 +1,38 @@
 package me.projectbw;
 
-import com.google.inject.Inject;
-import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
-import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BWTelegramNotifyVelocity {
-    private final ProxyServer server;
-    private final Logger logger;
-    private TelegramSender telegramSender;
+    private static final Logger logger = LoggerFactory.getLogger(BWTelegramNotifyVelocity.class);
+    private final ProxyServer proxyServer;
+    private final TelegramSender telegramSender;
 
-    @Inject
-    public BWTelegramNotifyVelocity(ProxyServer server, Logger logger) {
-        this.server = server;
-        this.logger = logger;
+    public BWTelegramNotifyVelocity(ProxyServer proxyServer, TelegramSender telegramSender) {
+        this.proxyServer = proxyServer;
+        this.telegramSender = telegramSender;
     }
 
     @Subscribe
     public void onProxyInitialize(ProxyInitializeEvent event) {
-        logger.info("\u001B[92m[PROJECTBW.RU] \u001B[93mBWTelegramNotify \u001B[32mАктивен\u001B[0m");
-
-        telegramSender = new TelegramSender();
-        telegramSender.sendMessage("⚡ Velocity сервер запущен!");
-
-        // Регистрируем команду /status
-        server.getCommandManager().register("status", new SimpleCommand() {
-            @Override
-            public void execute(Invocation invocation) {
-                invocation.source().sendMessage(Component.text("[TelegramNotify] Статус: Активен").color(NamedTextColor.GREEN));
-            }
-        });
+        logger.info("[PROJECTBW.RU] BWTelegramNotify Активен");
+        telegramSender.sendMessage("Сервер запущен!");
     }
 
     @Subscribe
     public void onPlayerJoin(PostLoginEvent event) {
-        String message = "✅ Игрок " + event.getPlayer().getUsername() + " зашел на сервер.";
-        telegramSender.sendMessage(message);
+        Player player = event.getPlayer();
+        telegramSender.sendMessage(String.format("Игрок %s зашел на сервер.", player.getUsername()));
     }
 
     @Subscribe
-    public void onPlayerLeave(DisconnectEvent event) {
-        String message = "❌ Игрок " + event.getPlayer().getUsername() + " вышел с сервера.";
-        telegramSender.sendMessage(message);
+    public void onPlayerQuit(PostLoginEvent event) {
+        Player player = event.getPlayer();
+        telegramSender.sendMessage(String.format("Игрок %s покинул сервер.", player.getUsername()));
     }
 }
