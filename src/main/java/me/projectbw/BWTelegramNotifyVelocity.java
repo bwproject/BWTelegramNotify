335edd9a3548b655add4016ba63ac669a4181012
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-14 10:58:05 +0500

    Update BWTelegramNotifyVelocity.java

--- src/main/java/me/projectbw/BWTelegramNotifyVelocity.java
+++ src/main/java/me/projectbw/BWTelegramNotifyVelocity.java
@@ -1,45 +1,43 @@
 package me.projectbw;
 
-import com.google.inject.Inject;
-import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.connection.DisconnectEvent;
-import com.velocitypowered.api.event.connection.LoginEvent;
-import com.velocitypowered.api.event.player.ServerConnectedEvent;
+import com.velocitypowered.api.event.EventHandler;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.connection.PreLogoutEvent;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Plugin(id = "bwtelegramnotify", name = "BWTelegramNotify", version = "1.0", authors = {"YourName"})
-public class BWTelegramNotifyVelocity {
+@Plugin(id = "bwtelegramnotify", name = "BWTelegramNotify", version = "1.0-SNAPSHOT", description = "Notify server status and players")
+public class BWTelegramNotifyVelocity implements SimpleCommand {
 
-    private final ProxyServer server;
-    private final Logger logger;
-    private TelegramSender telegramSender;
+    private static final Logger logger = LoggerFactory.getLogger(BWTelegramNotifyVelocity.class);
+    private final TelegramSender telegramSender;
 
-    @Inject
-    public BWTelegramNotifyVelocity(ProxyServer server, Logger logger) {
-        this.server = server;
-        this.logger = logger;
-    }
-
-    // Пустой конструктор для Guice (если нужно)
     public BWTelegramNotifyVelocity() {
-        this.server = null;
-        this.logger = null;
+        this.telegramSender = new TelegramSender(
+            "your_bot_token_here",
+            "your_chat_id_here"
+        );
     }
 
-    @Subscribe
-    public void onPlayerLogin(LoginEvent event) {
-        telegramSender.sendMessage("Игрок " + event.getPlayer().getUsername() + " зашел на сервер.");
+    @EventHandler
+    public void onPlayerJoin(PostLoginEvent event) {
+        Player player = event.getPlayer();
+        telegramSender.sendMessage("Игрок " + player.getUsername() + " присоединился к серверу");
     }
 
-    @Subscribe
-    public void onPlayerDisconnect(DisconnectEvent event) {
-        telegramSender.sendMessage("Игрок " + event.getPlayer().getUsername() + " вышел с сервера.");
+    @EventHandler
+    public void onPlayerQuit(PreLogoutEvent event) {
+        Player player = event.getPlayer();
+        telegramSender.sendMessage("Игрок " + player.getUsername() + " покинул сервер");
     }
 
-    @Subscribe
-    public void onServerChange(ServerConnectedEvent event) {
-        telegramSender.sendMessage("Игрок " + event.getPlayer().getUsername() + " сменил сервер на " + event.getServer().getServerInfo().getName());
+    @Override
+    public void execute(Invocation invocation) {
+        String status = telegramSender.checkBotStatus();
+        invocation.source().sendMessage(Component.text(status));
     }
 }
