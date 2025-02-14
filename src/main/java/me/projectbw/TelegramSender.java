d6a321ff1d9bb851894b4f8d92ab563d9e8ad90b
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-14 09:00:59 +0500

    Update TelegramSender.java

--- src/main/java/me/projectbw/TelegramSender.java
+++ src/main/java/me/projectbw/TelegramSender.java
@@ -1,52 +1,46 @@
 package me.projectbw;
 
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-
-import java.util.List;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TelegramSender extends TelegramLongPollingBot {
-    private final List<String> chatIds;
+    private final String botToken;
+    private final String chatId;
 
-    public TelegramSender(String botToken, List<String> chatIds) {
-        super(botToken);
-        this.chatIds = chatIds;
+    public TelegramSender(String botToken, String chatId) {
+        this.botToken = botToken;
+        this.chatId = chatId;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // Можно обработать команды из Telegram
     }
 
     @Override
     public String getBotUsername() {
-        return "YourBotUsername";
+        return "YourBotUsername"; // Укажите имя бота
     }
 
     @Override
     public String getBotToken() {
-        return "YOUR_BOT_TOKEN";
+        return botToken;
     }
 
-    public void sendMessage(String messageText) {
-        SendMessage message = new SendMessage();
-        message.setText(messageText);
-
-        for (String chatId : chatIds) {
-            message.setChatId(chatId);
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void sendMessageToChatId(String chatId, String messageText) {
-        SendMessage message = new SendMessage()
-                .setChatId(chatId)
-                .setText(messageText);
+    public void sendMessage(String message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(message);
         try {
-            execute(message);
+            execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    public String checkBotStatus() {
+        return "Бот работает";
     }
 }
