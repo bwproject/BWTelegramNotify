b6f009f8db84cd2f1455d93dfadfac3a1d84de80
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-14 11:21:16 +0500

    Update BWTelegramNotifyVelocity.java

--- src/main/java/me/projectbw/BWTelegramNotifyVelocity.java
+++ src/main/java/me/projectbw/BWTelegramNotifyVelocity.java
@@ -1,43 +1,41 @@
 package me.projectbw;
 
-import com.velocitypowered.api.event.EventHandler;
 import com.velocitypowered.api.event.Listener;
-import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent;
+import com.velocitypowered.api.event.handler.EventHandler;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.event.proxy.PlayerJoinEvent;
+import com.velocitypowered.api.event.proxy.PlayerLeaveEvent;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent.Response;
-import com.velocitypowered.api.command.Command;
-import com.velocitypowered.api.command.CommandSource;
-import com.velocitypowered.api.command.SimpleCommand;
 
-import org.slf4j.Logger;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+public class BWTelegramNotifyVelocity {
+    private final ProxyServer server;
+    private final TelegramSender telegramSender;
 
-public class BWTelegramNotifyVelocity implements Listener {
-    private TelegramSender telegramSender;
-    private TPSListener tpsListener;
-    private final Logger logger;
-
-    public BWTelegramNotifyVelocity(Logger logger) {
-        this.logger = logger;
+    public BWTelegramNotifyVelocity(ProxyServer server, TelegramSender telegramSender) {
+        this.server = server;
+        this.telegramSender = telegramSender;
     }
 
-    public void onEnable() {
-        // Цветной лог в консоль
-        logger.info("\u001b[32m[INFO] BWTelegramNotify плагин активен!");  // Зеленый цвет
-
-        // Инициализация и регистрация listener
-        this.telegramSender = new TelegramSender("your_bot_token", "your_chat_id");  // Пример значений
-        this.tpsListener = new TPSListener(telegramSender, 18.0, 60);  // Порог TPS 18 и интервал 60 секунд
-
-        // Регистрация TPSListener
-        this.getServer().getEventManager().register(this, this.tpsListener);
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String message = "Игрок " + player.getUsername() + " присоединился к серверу!";
+        telegramSender.sendMessage(message);
     }
 
-    public class StatusCommand implements SimpleCommand {
-        @Override
-        public void execute(Invocation invocation) {
-            String status = telegramSender.checkBotStatus();
-            invocation.source().sendMessage(status);
-        }
+    @EventHandler
+    public void onPlayerLeave(PlayerLeaveEvent event) {
+        Player player = event.getPlayer();
+        String message = "Игрок " + player.getUsername() + " покинул сервер.";
+        telegramSender.sendMessage(message);
+    }
+
+    @EventHandler
+    public void onProxyPing(ProxyPingEvent event) {
+        Response response = event.getResponse();
+        String message = "Статус сервера: " + response.getVersion().getName();
+        telegramSender.sendMessage(message);
     }
 }
