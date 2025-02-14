24e34246aa59b823568ec8cfc56bb0edcef8e03f
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-14 11:04:47 +0500

    Update TelegramSender.java

--- src/main/java/me/projectbw/TelegramSender.java
+++ src/main/java/me/projectbw/TelegramSender.java
@@ -6,7 +6,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TelegramSender extends TelegramLongPollingBot {
-
     private final String botToken;
     private final String chatId;
 
