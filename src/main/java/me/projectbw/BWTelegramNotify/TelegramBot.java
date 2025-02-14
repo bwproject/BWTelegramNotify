13120c1e9f94b2d869cbd14f7af3efb346dd0ccc
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-14 14:37:38 +0500

    Update TelegramBot.java

--- src/main/java/me/projectbw/BWTelegramNotify/TelegramBot.java
+++ src/main/java/me/projectbw/BWTelegramNotify/TelegramBot.java
@@ -1,19 +1,37 @@
 package me.projectbw.BWTelegramNotify;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.bots.AbsSender;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
-public class TelegramBot extends AbsSender {
+public class TelegramBot extends TelegramLongPollingBot {
+    private final String botUsername = "YourBotUsername";
+    private final String botToken = "YourBotToken";
 
     @Override
-    public <T> void sendApiMethod(T method) throws TelegramApiException {
-        if (method instanceof SendMessage) {
-            // Реализация отправки сообщения в Telegram
-            // Используйте ваше подключение и логику для отправки.
-            SendMessage message = (SendMessage) method;
-            // Логика отправки сообщения
-            System.out.println("Sending message: " + message.getText());
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // Логика обработки входящих сообщений
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            SendMessage message = new SendMessage();
+            message.setChatId(update.getMessage().getChatId());
+            message.setText("Received message: " + messageText);
+
+            try {
+                execute(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
