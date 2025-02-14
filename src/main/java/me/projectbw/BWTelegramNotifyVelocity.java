5f19cb627dfdf236d15d530a64b598f2505354ea
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-14 09:24:58 +0500

    Update BWTelegramNotifyVelocity.java

--- src/main/java/me/projectbw/BWTelegramNotifyVelocity.java
+++ src/main/java/me/projectbw/BWTelegramNotifyVelocity.java
@@ -1,49 +1,45 @@
 package me.projectbw;
 
+import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
-import com.velocitypowered.api.event.connection.PostLoginEvent;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
+import com.velocitypowered.api.event.connection.LoginEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-@Plugin(id = "bwtelegramnotify", name = "BWTelegramNotify", version = "1.0")
+@Plugin(id = "bwtelegramnotify", name = "BWTelegramNotify", version = "1.0", authors = {"YourName"})
 public class BWTelegramNotifyVelocity {
+
     private final ProxyServer server;
-    private final TelegramSender telegramSender;
-    private static final Logger logger = LoggerFactory.getLogger("BWTelegramNotify");
+    private final Logger logger;
+    private TelegramSender telegramSender;
 
-    public BWTelegramNotifyVelocity(ProxyServer server) {
+    @Inject
+    public BWTelegramNotifyVelocity(ProxyServer server, Logger logger) {
         this.server = server;
-        this.telegramSender = new TelegramSender("BOT_TOKEN", "CHAT_ID"); // Укажите токен
-        server.getEventManager().register(this, this);
+        this.logger = logger;
+    }
+
+    // Пустой конструктор для Guice (если нужно)
+    public BWTelegramNotifyVelocity() {
+        this.server = null;
+        this.logger = null;
     }
 
     @Subscribe
-    public void onProxyInitialize(ProxyInitializeEvent event) {
-        telegramSender.sendMessage("Сервер включен.");
-        logger.info("Сервер запущен.");
+    public void onPlayerLogin(LoginEvent event) {
+        telegramSender.sendMessage("Игрок " + event.getPlayer().getUsername() + " зашел на сервер.");
     }
 
     @Subscribe
-    public void onProxyShutdown(ProxyShutdownEvent event) {
-        telegramSender.sendMessage("Сервер выключен.");
-        logger.info("Сервер выключен.");
+    public void onPlayerDisconnect(DisconnectEvent event) {
+        telegramSender.sendMessage("Игрок " + event.getPlayer().getUsername() + " вышел с сервера.");
     }
 
     @Subscribe
-    public void onPlayerJoin(PostLoginEvent event) {
-        Player player = event.getPlayer();
-        telegramSender.sendMessage("Игрок " + player.getUsername() + " вошел на сервер.");
-    }
-
-    @Subscribe
-    public void onPlayerQuit(DisconnectEvent event) {
-        Player player = event.getPlayer();
-        telegramSender.sendMessage("Игрок " + player.getUsername() + " покинул сервер.");
+    public void onServerChange(ServerConnectedEvent event) {
+        telegramSender.sendMessage("Игрок " + event.getPlayer().getUsername() + " сменил сервер на " + event.getServer().getServerInfo().getName());
     }
 }
