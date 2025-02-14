e58b1e794f6db8ef5ef53a0b4327598bccfe442b
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-14 14:11:58 +0500

    Update BWTelegramNotifyVelocity.java

--- src/main/java/me/projectbw/BWTelegramNotify/BWTelegramNotifyVelocity.java
+++ src/main/java/me/projectbw/BWTelegramNotify/BWTelegramNotifyVelocity.java
@@ -1,76 +1,45 @@
 package me.projectbw.BWTelegramNotify;
 
 import com.velocitypowered.api.event.Listener;
-import com.velocitypowered.api.event.proxy.ProxyPingEvent;
+import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.PlayerJoinEvent;
 import com.velocitypowered.api.event.proxy.PlayerLeaveEvent;
 import com.velocitypowered.api.event.proxy.ServerPreConnectEvent;
-import com.velocitypowered.api.event.handler.EventHandler;
-import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
+public class BWTelegramNotifyVelocity implements Listener {
 
-@Plugin(id = "BWTelegramNotify", name = "BWTelegramNotify", version = "1.0-SNAPSHOT")
-public class BWTelegramNotifyVelocity {
     private final ProxyServer server;
-    private final Logger logger = LoggerFactory.getLogger(BWTelegramNotifyVelocity.class);
-    private TelegramBot telegramBot;
 
-    @Inject
     public BWTelegramNotifyVelocity(ProxyServer server) {
         this.server = server;
     }
 
-    @EventHandler
+    @Subscribe
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if (Config.get().notifications.player_login) {
-            telegramBot.sendMessage("Player " + event.getPlayer().getUsername() + " joined.");
-        }
+        // Отправка уведомления о входе игрока
+        // Используйте методы для отправки сообщений в Telegram
+        String playerName = event.getPlayer().getUsername();
+        sendTelegramMessage("Player " + playerName + " has joined the server.");
     }
 
-    @EventHandler
+    @Subscribe
     public void onPlayerLeave(PlayerLeaveEvent event) {
-        if (Config.get().notifications.player_logout) {
-            telegramBot.sendMessage("Player " + event.getPlayer().getUsername() + " left.");
-        }
+        // Отправка уведомления о выходе игрока
+        String playerName = event.getPlayer().getUsername();
+        sendTelegramMessage("Player " + playerName + " has left the server.");
     }
 
-    @EventHandler
-    public void onServerChange(ServerPreConnectEvent event) {
-        if (Config.get().notifications.server_change) {
-            telegramBot.sendMessage("Player " + event.getPlayer().getUsername() + " is switching to server " + event.getTarget().getName());
-        }
+    @Subscribe
+    public void onServerPreConnect(ServerPreConnectEvent event) {
+        // Отправка уведомления о смене сервера игроком
+        String playerName = event.getPlayer().getUsername();
+        String serverName = event.getServer().getServerInfo().getName();
+        sendTelegramMessage("Player " + playerName + " is connecting to server " + serverName + ".");
     }
 
-    @EventHandler
-    public void onServerPing(ProxyPingEvent event) {
-        if (Config.get().notifications.low_tps && server.getConfiguration().getTPS() < 15) {
-            telegramBot.sendMessage("Low TPS detected: " + server.getConfiguration().getTPS() + " TPS");
-        }
-    }
-
-    @EventHandler
-    public void onServerStart() {
-        telegramBot.sendMessage("Server started!");
-    }
-
-    @EventHandler
-    public void onServerStop() {
-        telegramBot.sendMessage("Server stopped!");
-    }
-
-    public void onEnable() {
-        telegramBot = new TelegramBot(Config.get().telegram.token);
-        telegramBot.start();
-        logger.info("BWTelegramNotify plugin enabled.");
-    }
-
-    public void onDisable() {
-        telegramBot.stop();
-        logger.info("BWTelegramNotify plugin disabled.");
+    private void sendTelegramMessage(String message) {
+        // Реализуйте отправку сообщения в Telegram
+        System.out.println("Sending message to Telegram: " + message);
     }
 }
