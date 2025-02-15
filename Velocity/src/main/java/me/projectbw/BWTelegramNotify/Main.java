966a745daeae6002df2b7614ebbd3a864c067071
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-15 18:54:56 +0500

    5.7.0

--- Velocity/src/main/java/me/projectbw/BWTelegramNotify/Main.java
+++ Velocity/src/main/java/me/projectbw/BWTelegramNotify/Main.java
@@ -1,23 +1,17 @@
 package me.projectbw.BWTelegramNotify;
 
-import com.velocitypowered.api.event.EventHandler;
-import com.velocitypowered.api.event.Listener;
+import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
-import com.velocitypowered.api.event.connection.PreLoginEvent;
-import com.velocitypowered.api.proxy.Player;
-import com.velocitypowered.api.proxy.Server;
-import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.kyori.adventure.text.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 
 import javax.inject.Inject;
 import java.util.logging.Logger;
 
-public class Main implements Listener {
+public class Main {
 
     private final ProxyServer server;
     private final TelegramBot bot;
@@ -30,7 +24,7 @@ public class Main implements Listener {
         this.logger = logger;
     }
 
-    @EventHandler
+    @Subscribe
     public void onPlayerJoin(PostLoginEvent event) {
         Player player = event.getPlayer();
         String playerName = player.getUsername();
@@ -38,11 +32,11 @@ public class Main implements Listener {
         // Сообщение о входе
         String message = "Игрок " + playerName + " зашел в сервер!";
         bot.sendMessageToTelegram(message);
-        
+
         logger.info("Игрок " + playerName + " присоединился.");
     }
 
-    @EventHandler
+    @Subscribe
     public void onPlayerQuit(DisconnectEvent event) {
         Player player = event.getPlayer();
         String playerName = player.getUsername();
@@ -53,7 +47,4 @@ public class Main implements Listener {
 
         logger.info("Игрок " + playerName + " покинул сервер.");
     }
-
-    // Можете использовать дополнительную логику для уведомлений
-    // например, для изменения серверов или низкого TPS
 }
