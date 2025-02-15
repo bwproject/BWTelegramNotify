8aeda234a65f8f98ea7f3ee316c9771b1e595b90
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-15 14:19:41 +0500

    Create ServerEvents.java

---
+++ src/me/projectbw/BWTelegramNotify/events/ServerEvents.java
@@ -0,0 +1,26 @@
+package me.projectbw.BWTelegramNotify.events;
+
+import com.velocitypowered.api.event.Listener;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.server.ServerPingEvent;
+import com.velocitypowered.api.event.server.ServerShutdownEvent;
+import me.projectbw.BWTelegramNotify.Notifier;
+
+public class ServerEvents {
+
+    private final Notifier notifier;
+
+    public ServerEvents(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    @Subscribe
+    public void onServerPing(ServerPingEvent event) {
+        notifier.sendServerStatusNotification("Server is up and running!");
+    }
+
+    @Subscribe
+    public void onServerShutdown(ServerShutdownEvent event) {
+        notifier.sendServerStatusNotification("Server is shutting down.");
+    }
+}
