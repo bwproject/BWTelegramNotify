9215dea32690ed0aeaf1dae971a3a700aa813c6a
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-15 14:18:44 +0500

    Update PlayerEvents.java

--- src/me/projectbw/BWTelegramNotify/events/PlayerEvents.java
+++ src/me/projectbw/BWTelegramNotify/events/PlayerEvents.java
@@ -1,1 +1,29 @@
+package me.projectbw.BWTelegramNotify.events;
+
+import com.velocitypowered.api.event.Listener;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.PlayerJoinEvent;
+import com.velocitypowered.api.event.player.PlayerQuitEvent;
+import me.projectbw.BWTelegramNotify.Notifier;
+
+public class PlayerEvents {
+
+    private final Notifier notifier;
+
+    public PlayerEvents(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    @Subscribe
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String playerName = event.getPlayer().getUsername();
+        notifier.sendPlayerNotification(playerName, "joined the server");
+    }
+
+    @Subscribe
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        String playerName = event.getPlayer().getUsername();
+        notifier.sendPlayerNotification(playerName, "left the server");
+    }
+}
 
