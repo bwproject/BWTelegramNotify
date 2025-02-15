6240e7ee31ba5fd5d7245e19658e373d1d75fa5b
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-15 16:08:23 +0500

    Update Main.java

--- src/main/java/me/projectbw/BWTelegramNotify/Main.java
+++ src/main/java/me/projectbw/BWTelegramNotify/Main.java
@@ -2,10 +2,13 @@ package me.projectbw.BWTelegramNotify;
 
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.event.EventHandler;
+import com.velocitypowered.api.event.Listener;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import javax.inject.Inject;
 
-@Plugin(id = "bwtelegramnotify", name = "BWTelegramNotify", version = "1.0-SNAPSHOT")
-public class Main {
+@Plugin(id = "bwtelegramnotify", name = "BWTelegramNotify", version = "1.0-SNAPSHOT", authors = {"YourName"})
+public class Main implements Listener {
 
     private final ProxyServer server;
 
@@ -14,11 +17,17 @@ public class Main {
         this.server = server;
     }
 
-    public void onEnable() {
+    // Метод, вызываемый при инициализации плагина
+    @EventHandler
+    public void onProxyInitialize(ProxyInitializeEvent event) {
         // Логика при старте плагина
         System.out.println("BWTelegramNotify plugin is enabled!");
+
+        // Здесь можно зарегистрировать обработчики событий, например:
+        server.getEventManager().register(this, this);
     }
 
+    // Метод, вызываемый при остановке плагина
     public void onDisable() {
         // Логика при остановке плагина
         System.out.println("BWTelegramNotify plugin is disabled!");
