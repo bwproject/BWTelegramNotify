96b2940f46ce524f94543deed2dccd3bb5121370
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-15 18:49:25 +0500

    Update Main.java

--- Velocity/src/main/java/me/projectbw/BWTelegramNotify/Main.java
+++ Velocity/src/main/java/me/projectbw/BWTelegramNotify/Main.java
@@ -1,23 +1,59 @@
 package me.projectbw.BWTelegramNotify;
 
-import com.velocitypowered.api.event.player.PlayerJoinEvent;
-import com.velocitypowered.api.event.player.PlayerQuitEvent;
-import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.EventHandler;
+import com.velocitypowered.api.event.Listener;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.connection.PreLoginEvent;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.Server;
+import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.proxy.ProxyServer;
 import net.kyori.adventure.text.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 
-@Plugin(id = "bwtelegramnotify", name = "BWTelegramNotify", version = "1.0")
-public class Main {
+import javax.inject.Inject;
+import java.util.logging.Logger;
 
-    @Subscribe
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        // Send message to Telegram about player join
-        System.out.println("Player joined: " + event.getPlayer().getUsername());
+public class Main implements Listener {
+
+    private final ProxyServer server;
+    private final TelegramBot bot;
+    private final Logger logger;
+
+    @Inject
+    public Main(ProxyServer server, TelegramBot bot, Logger logger) {
+        this.server = server;
+        this.bot = bot;
+        this.logger = logger;
     }
 
-    @Subscribe
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        // Send message to Telegram about player quit
-        System.out.println("Player quit: " + event.getPlayer().getUsername());
+    @EventHandler
+    public void onPlayerJoin(PostLoginEvent event) {
+        Player player = event.getPlayer();
+        String playerName = player.getUsername();
+
+        // Сообщение о входе
+        String message = "Игрок " + playerName + " зашел в сервер!";
+        bot.sendMessageToTelegram(message);
+        
+        logger.info("Игрок " + playerName + " присоединился.");
     }
+
+    @EventHandler
+    public void onPlayerQuit(DisconnectEvent event) {
+        Player player = event.getPlayer();
+        String playerName = player.getUsername();
+
+        // Сообщение о выходе
+        String message = "Игрок " + playerName + " покинул сервер!";
+        bot.sendMessageToTelegram(message);
+
+        logger.info("Игрок " + playerName + " покинул сервер.");
+    }
+
+    // Можете использовать дополнительную логику для уведомлений
+    // например, для изменения серверов или низкого TPS
 }
