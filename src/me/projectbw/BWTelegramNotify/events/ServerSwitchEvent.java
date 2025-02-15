8dcd8513a750173514db083ed21c01daf9238ac4
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-15 14:19:24 +0500

    Create ServerSwitchEvent.java

---
+++ src/me/projectbw/BWTelegramNotify/events/ServerSwitchEvent.java
@@ -0,0 +1,22 @@
+package me.projectbw.BWTelegramNotify.events;
+
+import com.velocitypowered.api.event.Listener;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.ServerSwitchEvent;
+import me.projectbw.BWTelegramNotify.Notifier;
+
+public class ServerSwitchEvent {
+
+    private final Notifier notifier;
+
+    public ServerSwitchEvent(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    @Subscribe
+    public void onServerSwitch(ServerSwitchEvent event) {
+        String playerName = event.getPlayer().getUsername();
+        String serverName = event.getPlayer().getCurrentServer().map(s -> s.getServerInfo().getName()).orElse("unknown");
+        notifier.sendServerSwitchNotification(playerName, serverName);
+    }
+}
