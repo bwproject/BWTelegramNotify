19dd5fd8bf78160d666e750c2c3a595bb8d7a2b2
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-02-17 16:30:53 +0500

    Update PaperMain.java

--- Paper/src/main/java/me/projectbw/BWTelegramNotify/PaperMain.java
+++ Paper/src/main/java/me/projectbw/BWTelegramNotify/PaperMain.java
@@ -9,18 +9,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PaperMain extends JavaPlugin implements Listener {
     private TelegramBot telegramBot;
+    private double lowTpsThreshold;
 
     @Override
     public void onEnable() {
         this.telegramBot = new TelegramBot();
-
+        
         // Загружаем конфиг
-        saveDefaultConfig(); // если конфиг не существует, он будет создан
+        saveDefaultConfig();
         String chatId = getConfig().getString("chat_id");
         String botToken = getConfig().getString("bot_token");
         telegramBot.setConfig(chatId, botToken);
 
+        // Получаем сообщения и порог TPS из конфигурации
+        String joinMessageTemplate = getConfig().getString("join_message");
+        String quitMessageTemplate = getConfig().getString("quit_message");
+        lowTpsThreshold = getConfig().getDouble("low_tps_threshold");
+
+        // Регистрация событий
         Bukkit.getPluginManager().registerEvents(this, this);
+        
+        // Запуск проверки TPS
+        getServer().getScheduler().runTaskTimer(this, this::checkTps, 0, 100); // Проверка каждую секунду
         getLogger().info("BWTelegramNotify for Paper has been enabled!");
     }
 
@@ -31,15 +41,24 @@ public class PaperMain extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        String message = "Player " + event.getPlayer().getName() + " has joined the server.";
+        String message = getConfig().getString("join_message").replace("{player}", event.getPlayer().getName());
         getLogger().info(message);
         telegramBot.sendMessage(message);
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        String message = "Player " + event.getPlayer().getName() + " has left the server.";
+        String message = getConfig().getString("quit_message").replace("{player}", event.getPlayer().getName());
         getLogger().info(message);
         telegramBot.sendMessage(message);
+    }
+
+    private void checkTps() {
+        double tps = getServer().getTPS()[0]; // Получаем средний TPS за последние 1 секунду
+        if (tps < lowTpsThreshold) {
+            String message = "Warning: Server TPS is low! Current TPS: " + tps;
+            getLogger().warning(message);
+            telegramBot.sendMessage(message);
+        }
     }
 }
