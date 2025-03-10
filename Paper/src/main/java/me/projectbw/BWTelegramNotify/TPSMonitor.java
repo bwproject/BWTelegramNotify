c4f22bf77a3c33e9c65f058daf0c5aaed5f6adb7
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-03-10 17:00:29 +0500

    Update TPSMonitor.java

--- Paper/src/main/java/me/projectbw/BWTelegramNotify/TPSMonitor.java
+++ Paper/src/main/java/me/projectbw/BWTelegramNotify/TPSMonitor.java
@@ -4,13 +4,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class TPSMonitor {
-    private final TelegramBot telegramBot;
     private final PaperMain plugin;
     private static final double TPS_THRESHOLD = 15.0;
 
-    public TPSMonitor(PaperMain plugin, TelegramBot telegramBot) {
+    public TPSMonitor(PaperMain plugin) {
         this.plugin = plugin;
-        this.telegramBot = telegramBot;
     }
 
     public void startMonitoring() {
@@ -21,7 +19,6 @@ public class TPSMonitor {
                 if (tps < TPS_THRESHOLD) {
                     String message = "Внимание: низкий TPS: " + tps;
                     Bukkit.getLogger().warning(message);
-                    telegramBot.sendMessage(message);
                 }
             }
         }.runTaskTimerAsynchronously(plugin, 0L, 1200L);
