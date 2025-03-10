c593bd5dcb47dda05f000e2bedee1498125462c4
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2025-03-10 17:58:37 +0500

    Update PaperMain.java

--- Paper/src/main/java/me/projectbw/BWTelegramNotify/PaperMain.java
+++ Paper/src/main/java/me/projectbw/BWTelegramNotify/PaperMain.java
@@ -6,10 +6,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.server.ServerLoadEvent;
-import org.bukkit.event.server.ServerStopEvent;
+import org.bukkit.event.server.ServerStopEvent;  // Правильный импорт
 import org.bukkit.plugin.java.JavaPlugin;
 import org.simpleyaml.configuration.file.YamlConfiguration;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -75,7 +74,7 @@ public class PaperMain extends JavaPlugin implements Listener {
 
     // Обработчик события остановки сервера
     @EventHandler
-    public void onServerStop(ServerStopEvent event) {
+    public void onServerStop(ServerStopEvent event) {  // Используем ServerStopEvent
         String message = config.getString("messages.server_stopped", "⛔ **Сервер {server} выключен!**")
                 .replace("{server}", getServerName());
         logger.info(message);
